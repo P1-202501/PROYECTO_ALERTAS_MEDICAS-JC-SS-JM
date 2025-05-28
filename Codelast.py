769e0b92d4c2f5fbcf89e5ff523127b6176e1a79
Jesus-Uces <CARRILLO.JESUS@UCES.EDU.CO>
2025-05-28 17:57:55 -0500

    logs

--- Codelast.py
+++ Codelast.py
@@ -4,3 +4,12 @@ from datetime import datetime
 import pandas as pd
 import os #Sirve para leer variables del sistema (como contraseñas).
 import logging  
+
+# Configuración del sistema de logs
+logging.basicConfig(
+    filename='registro_ejecucion.log',
+    level=logging.DEBUG,  # Captura todos los niveles (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S' #Define cómo se muestra la fecha y hora
+)
+# Configuración del servidor SMTP/Correo electrónico
