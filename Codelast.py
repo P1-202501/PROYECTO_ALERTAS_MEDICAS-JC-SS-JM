41eba1bf8f83cb393f875546bfaff9db33bf8d25
Jesus-Uces <CARRILLO.JESUS@UCES.EDU.CO>
2025-05-28 17:57:05 -0500

    2

---
+++ Codelast.py
@@ -0,0 +1,6 @@
+import smtplib
+from email.mime.text import MIMEText
+from datetime import datetime
+import pandas as pd
+import os #Sirve para leer variables del sistema (como contraseñas).
+import logging  
